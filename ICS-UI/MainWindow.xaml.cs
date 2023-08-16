0bc3da17f67b469e80de4d71c8951d9831ae531b
Miguel Salinas Gancedo <masalinas.gancedo@gmail.com>
2023-08-16 17:30:19 +0200

    first commit

---
+++ ICS-UI/MainWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Web.WebView2.Core;
+using Microsoft.Web.WebView2.Wpf;
+using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace PrinterPoCNative
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        // Server Printer PoC
+        private string _serverPrinterUrl = "http://localhost:5173";
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        
+        private void GoToProxy_Click(object sender, RoutedEventArgs e)
+        {
+            this.icsWebBrowserView.Source = new Uri(_serverPrinterUrl);
+        }
+    }
+}
